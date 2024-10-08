3d8e88a8dde99a284a48188da198d25f31ce72e7
w-k-f-g <15901172577@163.com>
2024-10-08 17:51:29 +0100

    after lab3

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -828,7 +828,7 @@ public class Game {
      * and then run this class.
      */
     public static void main(String[] args) {
-        String gameType = Utils.getArg(args, "game", "Pandemic");
+        String gameType = Utils.getArg(args, "game", "CantStop");
         boolean useGUI = Utils.getArg(args, "gui", true);
         int turnPause = Utils.getArg(args, "turnPause", 0);
         long seed = Utils.getArg(args, "seed", System.currentTimeMillis());
@@ -836,8 +836,9 @@ public class Game {
 
         /* Set up players for the game */
         ArrayList<AbstractPlayer> players = new ArrayList<>();
-        players.add(new RandomPlayer());
-        players.add(new RandomPlayer());
+        players.add(new HumanGUIPlayer(ac));
+//        players.add(new RandomPlayer());
+//        players.add(new RandomPlayer());
         players.add(new BasicMCTSPlayer());
 
 //        RMHCParams params = new RMHCParams();
